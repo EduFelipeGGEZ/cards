0e8772119b2d466b94bd816b148bc877e2a446ab
EduFelipeGGEZ <eduardo.pedra@escola.pr.gov.br>
2025-08-01 09:49:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criacartao(
+    "Programação",
+    'O que é Javascript?',
+    'O javascript é uma linguagen de programação'
+)
+    criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraná',
+    'A capital do Paraná é Curitiba'
+)
+    criacartao(
+    'Programação',
+    'O que é Python?',
+    'o Python é uma linguagen de programção'
+)
